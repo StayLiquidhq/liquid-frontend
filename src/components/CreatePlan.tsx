deb6ce8a2a94d1e4bd5f7349c4caf7c49fe3a59b
clement <clementakhimien25@gmail.com>
2025-09-11 21:01:06 +0100

    moved up

--- src/components/CreatePlan.tsx
+++ src/components/CreatePlan.tsx
@@ -37,6 +37,25 @@ const CreatePlan: React.FC<CreatePlanProps> = ({ onClose, onPlanCreated }) => {
       <h2 className="text-2xl font-medium">Create a vault</h2>
 
       <div
+        onClick={() => setSelectedPlan("target")}
+        className="flex p-3 items-start gap-[10px] self-stretch squircle squircle-[18px] squircle-smooth-xl squircle-[#252525] cursor-pointer"
+      >
+        <div className="flex p-2 justify-center items-center squircle squircle-[6px] squircle-smooth-xl squircle-[#FF692C]">
+          <Image src="/target.svg" alt="Piggy Vest" width={72} height={72} />
+        </div>
+        <div className="flex flex-col">
+          <h3 className="text-lg font-medium">Piggy Vest</h3>
+          <p className="text-sm text-gray-400">
+            Set target amount and payout account or wallet, auto-paid when
+            target reached you can break with a 10% break fee,{" "}
+            <a href="#" className="text-blue-400">
+              learn more
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div
         onClick={() => setSelectedPlan("locked")}
         className="flex p-3 items-start gap-[10px] self-stretch squircle squircle-[18px] squircle-smooth-xl squircle-[#252525] cursor-pointer"
       >
@@ -74,25 +93,6 @@ const CreatePlan: React.FC<CreatePlanProps> = ({ onClose, onPlanCreated }) => {
             Deposit, set amount you want to receive, when and where. You can
             change payout wallet or account and break this vault with a 10%
             break fee,{" "}
-            <a href="#" className="text-blue-400">
-              learn more
-            </a>
-          </p>
-        </div>
-      </div>
-
-      <div
-        onClick={() => setSelectedPlan("target")}
-        className="flex p-3 items-start gap-[10px] self-stretch squircle squircle-[18px] squircle-smooth-xl squircle-[#252525] cursor-pointer"
-      >
-        <div className="flex p-2 justify-center items-center squircle squircle-[6px] squircle-smooth-xl squircle-[#FF692C]">
-          <Image src="/target.svg" alt="Piggy Vest" width={72} height={72} />
-        </div>
-        <div className="flex flex-col">
-          <h3 className="text-lg font-medium">Piggy Vest</h3>
-          <p className="text-sm text-gray-400">
-            Set target amount and payout account or wallet, auto-paid when
-            target reached you can break with a 10% break fee,{" "}
             <a href="#" className="text-blue-400">
               learn more
             </a>
